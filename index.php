479e1be9df9f9c6d6bb68195b78d803ec02a2f21
Nugget <cdl.040404@gmail.com>
2021-06-18 09:09:37 +0200

    require different pages depending on get variable

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
 </head>
 
 <body>
+    <?php
 
+    $pagina = isset($_GET["pagina"]) ? $_GET["pagina"] : null;
+
+    switch ($pagina) {
+        case "login":
+            require("./login.php");
+            break;
+        default:
+            require("./login.php"); // On first load, when no page is selected.
+    }
+
+    ?>
 </body>
 
 </html>
