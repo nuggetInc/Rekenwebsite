b8363f1161e94e8f8693fcdeeb5b6451ad73645b
Nugget <cdl.040404@gmail.com>
2021-06-18 12:33:27 +0200

    preset for page

--- index.php
+++ index.php
@@ -1,24 +1,49 @@
+<?php
+
+$pagina = isset($_GET["pagina"]) ? $_GET["pagina"] : null;
+
+?>
 <!DOCTYPE html>
 <html lang="nl">
 
 <head>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/index.css">
     <title>Rekenwebsite</title>
 </head>
 
 <body>
-    <?php
+    <header class="page-header">
+        <nav class="page-nav">
+            <!-- depends on logged in and page -->
+            <ul class="nav-list">
+                <li><a class="nav-item active" href="">home</a></li>
+                <li><a class="nav-item" href="">home</a></li>
+                <li><a class="nav-item" href="">home</a></li>
+                <li><a class="nav-item" href="">home</a></li>
+            </ul>
+        </nav>
+        <h2 class="page-name">
+            <!-- name of the logged in user -->
+            Nugget
+        </h2>
+        <h1>
+            <a class="page-title" href="./">REKENWEBSITE</a>
+        </h1>
+    </header>
+    <main class="page-content">
+        <?php
 
-    $pagina = isset($_GET["pagina"]) ? $_GET["pagina"] : null;
+        // switch ($pagina) {
+        //     case "login":
+        //         require("./login.php");
+        //         break;
+        //     default:"); // On first load, when no page is selected.
+        // }
+        //         require("./login.php
 
-    switch ($pagina) {
-        case "login":
-            require("./login.php");
-            break;
-        default:
-            require("./login.php"); // On first load, when no page is selected.
-    }
-
-    ?>
+        ?>
+    </main>
 </body>
 
 </html>
