3d03c6709ae05679274531f6b0abb076c0ed344b
Nugget <cdl.040404@gmail.com>
2021-06-18 13:50:43 +0200

    error handling for when no user is logged in

--- index.php
+++ index.php
@@ -5,11 +5,15 @@ session_start();
 
 $pagina = isset($_GET["pagina"]) ? $_GET["pagina"] : null;
 
-$parameters = array(":id" => $_SESSION["user_id"]);
-$sth = Database::prepare("SELECT naam FROM gebruiker WHERE id = :id");
-$sth->execute($parameters);
+$loggedIn = isset($_SESSION["user_id"]);
 
-$loggedInUser = $sth->fetch();
+if ($loggedIn) {
+    $parameters = array(":id" => $_SESSION["user_id"]);
+    $sth = Database::prepare("SELECT naam FROM gebruiker WHERE id = :id");
+    $sth->execute($parameters);
+
+    $loggedInUser = $sth->fetch();
+}
 
 ?>
 <!DOCTYPE html>
@@ -34,7 +38,7 @@ $loggedInUser = $sth->fetch();
         </nav>
         <h2 class="page-name">
             <!-- name of the logged in user -->
-            <?= $loggedInUser["naam"] ?>
+            <?= $loggedIn ? $loggedInUser["naam"] : null ?>
         </h2>
         <h1>
             <a class="page-title" href="./">REKENWEBSITE</a>
