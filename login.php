c89627e846f99fd86c91666ac8c8012183b4fe63
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-06-18 12:34:33 +0200

    login page push-01

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php session_start();
+
+$pdo = new PDO("mysql:host=localhost;dbname=Rekenwebsite", "root", "");
+
+if (isset($_POST["password"], $_POST["username"]))
+{
+    $salt = hash('sha512', $_POST["password"]);
+
+    $password =  hash('sha512',  $_POST["password"] . $salt);
+
+
+    $parameters = array(':naam'=>$_POST["username"], ':wachtwoord'=>$password);
+    $sth = $pdo->prepare("SELECT id FROM gebruiker WHERE naam = :naam AND wachtwoord = :wachtwoord");
+    $sth->execute($parameters);
+
+
+    if ($sth->rowCount()!= 0) // if user and password both exist;
+    {
+        $_SESSION["user_salt"] = $salt;
+        echo "BOB";
+    }
+    // header("Location: ./");
+}
+
+?>
+<form action="" method="POST">
+    <input type="text" placeholder="Naam" name="username" required>
+    <input type="password" placeholder="Wachtwoord" name="password" required>
+    <input type="submit">
+</form>
