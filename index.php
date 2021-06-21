1c6e990f13e41c59aeaf5ebfa147996e6e2dc4fc
Dustin van de Veerdonk <dveerdonk@gmail.com>
2021-06-21 11:17:03 +0200

    Login div style

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ if ($loggedIn) {
 <head>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/login.css">
     <title>Rekenwebsite</title>
 </head>
 
